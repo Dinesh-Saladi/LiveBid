db781166635ad21f1fead50af9e4d4827623db8f
Dinesh <dineshsaladi79@gmail.com>
2025-06-05 17:59:06 +0530

    v1 changed again

--- frontend/src/components/my_components/HomePage/Hero.jsx
+++ frontend/src/components/my_components/HomePage/Hero.jsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-import pattern from "../assets/pattern.svg";
+import pattern from "../../../assets/pattern.svg";
 
 function Hero() {
   return (
